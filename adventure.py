a6a092b1fe1e8250f70a2e21683a12bd809b67eb
yamilamcl <ymclaughlin@brooklynfriends.org>
2019-10-18 00:59:42 +0000

    message

--- adventure.py
+++ adventure.py
@@ -39,8 +39,10 @@ def setup():
     global name
     global HP
     global MP
+    global weapons
+    global text2art
     #Our variable "name" is used to store our name, captured by keyboard input.
-    name = input("What is your name, gingerbread man?")
+    name = input("What is your name, gingerbread person?")
     #randint is a great way of adding some variety to your players statistics through randomness
     HP = randint(5,20)
     MP = randint(5,20)
@@ -48,20 +50,26 @@ def setup():
 def villager():
     #This will create a randomly named Villager to interact with
     global npcname
-    global response
+    global responses
     #Below is a list, we can store lots of things in a list and then retrieve them later.
-    responses = ["Hi", "Are you a hero?", "Are you from this village?", "There has been a dark shadow cast across the village"]
-    npcnamechoice = ["Roger", "Dexter", "Sarah", "Susan"]
+    responses = ["Beware the chocolate ogre he haunts this beautiful and delicious town", "Are you a hero?", "There has been a chocolate ogre terrorizing the village"]
+    npcnamechoice = ["Oreo", "Hot Sauce", "Sandwhich", "Pasta"]
     #Shuffle will shuffle the list contents into a random order.
     shuffle(npcnamechoice)
     npcname = npcnamechoice[0]
     print ("\n["+npcname+":] Hello, my name is "+npcname+", Would you like to talk to me?\n")
     shuffle(responses)
-    print ("Press y to talk to the villager")
+    print ("Press y to talk to the oompa loompa")
     if input() == "y":
         print ("%s: %s" % (npcname, responses[0]))
     else:
         print ("%s: Goodbye" % npcname)
+    print ("If you want to learn more about the Chocolate Ogre ask the oompa loompa by pressing c and enter. If you want to ignore the oompa loompa and continue on your journey, press i and continue")
+    if input() == "c":
+        print (""+npcname+" then explains to you how the chocolate Ogre recently came upon their town and has been sleeping in their sacred chocolate river. He is starting to kill all of the oompa loompa's friends and they are continously begging him to leave but no one is brave enough to stop him. You then explain to him how you will keep a look out and be careful incase you come across the ogre.")
+    
+    if input() == "i":
+        print ("Thanks for the warning, but I'm sure I will be fine.")
 
 def enemy():
     global enemyHP
@@ -70,10 +78,13 @@ def enemy():
     enemyHP = randint(5,20)
     enemyMP = randint(5,20)
     #Below is the enemy's name, perhaps you could change this to a list and then shuffle the list, such as we did for the villager above.
-    enemyname = "Ogre"
-    print ("\nSuddenly you hear a roar, and from the shadows you see an "+enemyname+" coming straight at you....")
+    enemynamechoice = ["Chocolate Blob Bob", "Hot Fudge Sludge", "Bad Mad Rad Chocolate Dad", "Annoying Big Ball of Chocolate"]
+    #Shuffle will shuffle the list contents into a random order.
+    shuffle(enemynamechoice)
+    enemyname = enemynamechoice [0]
+    print ("\nSuddenly you hear a gurgle, and from the shadows you see an "+enemyname+" rolling straight at you....")
     #print enemyname
-    print ("Your enemy has %s Health Points" % str(enemyHP))
+    print (""+enemyname+" has %s Health Points" % str(enemyHP))
     print ("Your enemy has %s Magic Points" % str(enemyMP))
 
 
@@ -88,7 +99,7 @@ global HP
 global MP
 global move
 global enemyHP
-print ("Welcome to the land of Delicacies, %s" % name)
+print ("Welcome to the Safe Haven of Delicacies, %s" % name)
 #Sleep is Python's way of pausing the game for a specified number of seconds
 sleep(2)
 #Below we are using the helper functions to join a string of text to an integer via the str() helper.
@@ -97,32 +108,32 @@ print ("Your magic skill is" + " " + str(MP))
 
 
 
-print ("Would you like to venture out into the land? Press y then enter to continue")
+print ("Would you like to venture out into the Haven? Press y then enter to continue")
 #Below we use input to ask for user input, and if it is equal to y, then the code underneath is run.
 if input() == "y":
-    print ("You are in your home, with a roaring fireplace in front of you, above the fire you can see your baguette and pizza")
-    print ("Would you like to take your baguette and pizza or would you like to take your marshmallow gun? To take your baguette and pizza, press y then enter to continue. If you want to take the marshmallow gun press m.")
+    print ("You are in your burger shaped home that you built to express your love for burgers. You are seated in a comfy marshmallow chair with your hot dog dog on your lap. There is a roaring graham cracker fireplace in front of you, and above the fire you can see your baguette, pizza and marshmallow gun.")
+    print ("Would you like to take your baguette and pizza or would you like to take your marshmallow gun? To take your baguette and pizza, press y then enter to continue. If you want to take the marshmallow gun press m and enter to continue.")
     if input() == "y":
         #This is a list, and it can store many items, and to do that we "append" items to the list.
         weapons = []
         weapons.append("baguette")
         weapons.append("pizza")
         print ("You are now carrying your %s and your %s" % (weapons[0], weapons[1]))
-        print ("Armed with your %s and %s you swing open the door to your delicious candy home and see a vast vally of licorice string gleaming in the sunshine." % (weapons[0], weapons[1]))
+        print ("Armed with your %s and %s you swing open the door to your delicious burger home and see a vast vally of licorice string gleaming in the sunshine." % (weapons[0], weapons[1]))
     if input () == "m":
         weapons = []
         weapons.append("marshmallow gun")
         print ("You are now carrying your %s" % (weapons[2]))
-        print ("Armerd with your %s you swing open the door to your delicous candy home and see a vast valley of licorice string gleaming in the sunshine." % (weapon[2]))
+        print ("Armed with your %s you swing open the door to your delicous burger home and see a vast valley of licorice string gleaming in the sunshine." % (weapons[2]))
     else:
         print ("You choose not to take your weapons")
-        print ("Armed with your sense of humour, You swing open the door to see a green valley full of opportunity awaiting you.")
+        print ("Armed with your sense of humour, You swing open the door to see a green valley full of licorice awaiting you.")
 else:
-    print ("You stay at home, sat in your favourite chair watching the fire grow colder. The land of Narule no longer has a hero.")
+    print ("You stay at home, sat in your favourite marshmallow chair with your hot dog dog watching the fire grow colder. The Safe Haven of Delacacies no longer has a hero.")
     print ("Game Over")
     sys.exit(0)
 
-print ("In the distance to the north you can see a small village, to the east you can see a river and to the west a field of wild flowers.")
+print ("In the distance to the north you can see a small with litte people that you have never gotten a chance to visit, to the east you can see a chocolate river and to the west a field of wild french fries.")
 
 #Remember those functions we created at the start of the code? Well here we are using them in the game.
 print ("\n")
@@ -132,14 +143,14 @@ west()
 move = input("Where would you like to go? ")
 if move == 'n':
     print ("\nYou move to the north, walking in the sunshine.")
-    print ("A villager is in your path and greets you")
+    print ("A oompa loompa is in your path and greets you")
 #elif is short for Else If and it means that if the previous condition is false, to check this condition to see if that is true.
 elif move == 'e':
-    print ("\nYou walk to the river which lies to the east of your home.")
-    print ("A villager is in your path and greets you")
+    print ("\nYou walk to the chocolate river which lies to the east of your home.")
+    print ("A oompa loompa is in your path and greets you")
 elif move == 'w':
-    print ("\nYou walk to the field of wild flowers, stopping to take in the beauty")
-    print ("A villager is in your path and greets you\n")
+    print ("\nYou walk to the field of wild french fries, stopping to chomp on a few")
+    print ("A oompa loompa is in your path and greets you\n")
 
 villager()
 enemy()
@@ -148,20 +159,23 @@ sleep(3)
 fight = input("Do you wish to fight?" )
 
 if fight == "y":
-    while HP > 0:
-#This loop will only work while our characters HP is greater than 0.
-        hit = randint(0,5)
-        print ("You swing your baguette and cause %s of damage" % str(hit))
+    while HP > -5:
+#This loop will only work while our characters HP is greater than -5.
+        hit = randint(5,10)
+        print ("You use your weapon and cause %s of damage" % str(hit))
         enemyHP = enemyHP - hit
         print (enemyHP)
-        enemyhit = randint(0,5)
+        enemyhit = randint(0,3)
         print ("The ogre swings a club at you and causes %s of damage" % str(enemyhit))
         HP = HP - enemyhit
         print (HP)
 else:
-    print ("You turn and run away from the ogre")
+    print ("You turn and run away from the menacing chocolate ogre")
 
-print ("This is where this template ends, this is now YOUR world, build your adventure and share it with the world")
+print ("You have finally killed the menacing and evil Ogre and the Safe Haven of Delacacies has returned to peace.")
+print ("As you walk through the village on your way home you tell the oompa loompa's of your battle. They cheer in excitement and gratitude and invite you to a delicious buffet down by the Chocolate River, with your favorite food... Burgers. ")
+print ("As you walk down to the River where your delicious dinner awaits you get to talking to the oopma loompas and find your friend "+npcname+" and you become bestfriends")
+print ("Then you live happily ever after with your oompa loompa friends, in the Safe Haven you now call home.")
 
 print ("   _       _                 _")
 print ("  /_\   __| |_   _____ _ __ | |_ _   _ _ __ ___")
